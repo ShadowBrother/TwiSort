d58ae286b2f5952f104e17b05d484682f8b4ed86
Jesse Hoyt <jesselhoyt@gmail.com>
2016-04-13 00:56:39 -0400

    Sorting Accuracy Dislpayed -Now calculates accuracy of sorting -Now calculates average time to sort each zip -Fixed bug in TopYellow button caused by typo

--- Assets/TwiSort.cs
+++ Assets/TwiSort.cs
@@ -11,16 +11,22 @@ public class TwiSort : MonoBehaviour {
 
     public int zip;//zip code to sort
     public Text textbox;//textbox to display info
+    public Text accuracyDisplay;//textbox to display accuracy info
     public int low;//low range for zip
     public int high;//high range for zip
+
+    public int numTries { get; private set; }//Number of zip codes user has tried to sort
+    public int numCorrect { get; private set; }//Number of zip codes user has correctly sorted
+    public float timeStart { get; private set; }//Time when random zip is shown, for calculating time to sort
+    public float timeTotal { get; private set; }//Total time spent sorting, for calculating average time to sort
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start");
         UnityEngine.Random.seed = (int)Time.time * (int)Time.frameCount;//seed Random
-        Debug.Log(Time.time);
-        Debug.Log(Time.frameCount);
-        Debug.Log(UnityEngine.Random.seed);
+        //Debug.Log(Time.time);
+        //Debug.Log(Time.frameCount);
+        //Debug.Log(UnityEngine.Random.seed);
 	}
 	
 	// Update is called once per frame
@@ -32,6 +38,7 @@ public class TwiSort : MonoBehaviour {
         Debug.Log(UnityEngine.Random.seed);
     }
 
+    //returns the Belt a package with zip should be sorted to
     public Belt sort(int zip)
     {
 
@@ -125,25 +132,39 @@ public class TwiSort : MonoBehaviour {
 
     public void randomZipHandler()
     {
-        int rand = randomZip(low, high);
-        textbox.GetComponent<Text>().text = "Zip Code: " + rand;
+        int rand = randomZip(low, high);//get random zip between low and high
+        textbox.GetComponent<Text>().text = "Zip Code: " + rand;//show zip code in textbox
         zip = rand;
+        timeStart = Time.time;
     }
 
     public void guess(string theGuess)
     {
-        
-        
+
+
+        float timeToGuess = Time.time - timeStart;//calculate how long it took to sort
+        timeTotal += timeToGuess;//increment timeTotal
+
         Belt answer = sort(zip);
         if(answer == (Belt)Belt.Parse(typeof(Belt),theGuess))//correct guess
         {
             textbox.GetComponent<Text>().text = "Correct! " + answer.ToString();
+            numCorrect++;//increment number of correctly sorted zips
         }
         else
         {
             textbox.GetComponent<Text>().text = "Wrong! Correct Belt was " + answer.ToString();
         }
 
+        numTries++;//increment number of zips attempted
+        updateAccuracyDisplay();
     }
 
+    //calculate averages and update display
+    void updateAccuracyDisplay()
+    {
+        float averageCorrect = (float)numCorrect / (float)numTries * 100;
+        float averageTime = timeTotal / numTries;
+        accuracyDisplay.GetComponent<Text>().text = string.Format("Sorting Accuracy: {0}%\n Average time to sort: {1} seconds",averageCorrect, averageTime);
+    }
 }
