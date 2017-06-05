4356e5438c98d8a5e4c38cc6ebd6310b53538daf
ShadowBrother <jesselhoyt@gmail.com>
2017-06-05 19:20:47 -0400

    dbTest -added a scene in Unity to test db interaction --has button that queries the db, ---if table has row, it displays value in a text boxif ---else if db empty it adds a row and then displays value

--- Assets/DatabaseInitialization.cs
+++ Assets/DatabaseInitialization.cs
@@ -1,16 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Data;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Mono.Data.Sqlite;
 
 public class DatabaseInitialization : MonoBehaviour {
 
+    private IDbConnection dbcon;
+    private IDbCommand dbcmd;
+
+    private SqliteConnection sqlcon;
+    private SqliteCommand sqlcmd;
+
+
+
+    public Text textBox;
+
 	// Use this for initialization
 	void Start () {
-		
+        //set up connection to database
+        string con = "URI=file:" + Application.dataPath + "/Databases/TwiSort";//path to db
+        //dbcon = (IDbConnection)new SqliteConnection(con);
+        //dbcon.Open();
+        //dbcmd = dbcon.CreateCommand();
+        Debug.Log(con);
+        sqlcon = new SqliteConnection(con);
+        sqlcon.Open();
+        sqlcmd = sqlcon.CreateCommand();
+
 	}
+
+    //dbTest tests reading/writing to a db
+   public void dbTest()
+    {
+
+        string sqlQuery = "SELECT HR, sort From [2Day]";
+        string sqlInsert = "INSERT INTO [2Day](HR, sort) VALUES(270, 'BottomGreen')";
+
+        sqlcmd.CommandText = sqlQuery;
+        SqliteDataReader reader = sqlcmd.ExecuteReader();
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                textBox.text += reader[0] + "\n";
+            }
+        }
+        else
+        {
+            Debug.Log("No rows Found.");
+            reader.Close();
+            sqlcmd.CommandText = sqlInsert;
+            sqlcmd.ExecuteNonQuery();
+            sqlcmd.CommandText = sqlQuery;
+            reader = sqlcmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    textBox.text += reader[0] + "\n";
+                }
+            }
+            else
+            {
+                textBox.text = "Something's wrong!";
+            }
+        }
+        reader.Close();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    //Clean up
+    private void OnDestroy()
+    {
+        sqlcmd.Dispose();
+        sqlcmd = null;
+        sqlcon.Close();
+        sqlcon = null;
+    }
 }
